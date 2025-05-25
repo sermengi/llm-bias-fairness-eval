7ec194e4d59823cf99efa314821257d64dcb28bb
sermengi <mengiserkan@outlook.com>
2025-05-25 11:44:27 -0400

    added logging functionality for data loader

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,4 +1,5 @@
 from datasets import load_dataset
+from src import logging
 
 
 class GSM_MC_PromptBuilder:
@@ -15,14 +16,22 @@ class GSM_MC_PromptBuilder:
             self.dataset = load_dataset(
                 self.dataset_name, data_files=self.data_files, split=self.split
             )
+            logging.info(f"{self.dataset_name} is loaded successfully.")
         except Exception as e:
-            raise e
+            logging.error(f"Failed to load dataset {self.dataset_name}. Error: {e}")
+            raise ValueError(
+                "Please check the dataset configurations: dataset name, split, or file path."
+            )
 
         if self.max_samples is not None:
             if self.max_samples > len(self.dataset):
-                print("Not enough sample in dataset")
+                logging.warning(
+                    f"{self.dataset_name} doesn't have {self.max_samples} samples in {self.split} split. Collecting all the available samples: {len(self.dataset)}"
+                )
             else:
-                print("Successfully retrieved specified number of samples")
+                logging.info(
+                    f"Successfully retrieved {self.max_samples} number of samples"
+                )
                 self.dataset = self.dataset.select(range(self.max_samples))
 
     def format_sample(self, sample, answer=None):
@@ -43,13 +52,17 @@ class GSM_MC_PromptBuilder:
     def get_sample_prompt(self, index):
         try:
             sample = self.dataset[index]
-        except IndexError:
-            raise IndexError(
+        except IndexError as e:
+            logging.error(
                 f"Index {index} is out of bounds for dataset of size {len(self.dataset)}."
             )
+            raise e
 
         prompt = self.format_sample(sample=sample, answer=sample["Answer"])
         return prompt
 
     def generate_prompts(self):
-        return [self.format_sample(sample) for sample in self.dataset]
+        logging.info("Generating prompts for all samples...")
+        prompts = [self.format_sample(sample) for sample in self.dataset]
+        logging.info(f"Generated {len(prompts)} prompts.")
+        return prompts
