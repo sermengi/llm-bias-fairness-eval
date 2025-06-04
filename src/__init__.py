d802e24d149e51a8601823c40c21fe90b70c6ffe
sermengi <mengiserkan@outlook.com>
2025-06-04 19:06:19 +0000

    modified logger for creating different log files, added hook for cleaning notebooks

--- src/__init__.py
+++ src/__init__.py
@@ -1,17 +1,20 @@
 import logging
 import os
 import sys
+from datetime import datetime
+
+LOG_FILE = datetime.now().strftime("%m_%d_%Y_%H_%M_%S") + ".log"
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 log_formatter = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
-log_dir = "logs"
-log_filepath = os.path.join(log_dir, "logging.log")
-os.makedirs(log_dir, exist_ok=True)
 
 logging.basicConfig(
     level=logging.INFO,
     format=log_formatter,
     handlers=[
-        logging.FileHandler(log_filepath),
+        logging.FileHandler(LOG_FILE_PATH),
         logging.StreamHandler(sys.stdout),
     ],
 )
