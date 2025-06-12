adfb4228699d275806ad751d1ef0a85b76d054fe
sermengi <mengiserkan@outlook.com>
2025-06-12 19:07:35 +0000

    added function for getting all the configs from yaml

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 from pydantic import BaseModel, ValidationError
 
 from src import logger
@@ -44,6 +46,7 @@ class ConfigurationManager:
                 split=config.split,
                 max_samples=config.max_samples,
             )
+            logger.info("Dataset configurations retrieved successfully.")
         except ValidationError as e:
             logger.error(f"Dataset configuration is not valid: \n{e}")
 
@@ -58,19 +61,21 @@ class ConfigurationManager:
                 batch_size=config.batch_size,
                 tokenizer_padding_side=config.tokenizer_padding_side,
             )
+            logger.info("Model configurations retrieved successfully.")
             return model_config
         except ValidationError as e:
             logger.error(f"Model configuration is not valid: \n{e}")
 
     def get_artifact_configuration(self) -> ArtifactConfig:
         config = self.config.artifact_configs
-        create_directory(config.artifacts_root)
+        create_directory(config.artifacts_root, verbose=False)
         try:
             artifact_config = ArtifactConfig(
                 artifacts_root=config.artifacts_root,
                 prediction_file_path=config.prediction_file_path,
                 mlflow_run_id=config.mlflow_run_id,
             )
+            logger.info("Artifact configurations retrieved successfully.")
             return artifact_config
         except ValidationError as e:
             logger.error(f"Artifact configuration is not valid: \n{e}")
@@ -82,6 +87,21 @@ class ConfigurationManager:
                 contexts=self.context_config.contexts,
                 identity_formatting=self.context_config.identity_formatting,
             )
+            logger.info("Context configurations retrieved successfully.")
             return context_config
         except ValidationError as e:
             logger.error(f"Context configuration is not valid: \n{e}")
+
+    def get_all_configurations(self) -> Dict:
+        logger.info("Retrieving all available configurations...")
+        dataset_config = self.get_dataset_configuration()
+        model_config = self.get_model_configuration()
+        artifact_config = self.get_artifact_configuration()
+        context_config = self.get_contexts_configuration()
+
+        return {
+            "dataset": dataset_config,
+            "model": model_config,
+            "artifact": artifact_config,
+            "context": context_config,
+        }
